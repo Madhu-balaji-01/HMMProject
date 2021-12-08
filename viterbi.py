3f0ae9ae45083c92ac7a117c894e274e1c4dc4d2
Madhumitha Balaji <65944319+Madhu-balaji-01@users.noreply.github.com>
2021-12-08 19:11:10 +0800

    Debugging

--- viterbi.py
+++ viterbi.py
@@ -6,12 +6,10 @@ from utils import *
 def viterbi(emission_counts, transition_counts, observations, train_obs):
     # Retrieving a list of all possible states
     states = list(transition_counts.keys())
-    states.remove('START')
-    # states.append('STOP') # START is already in the list, no need to append
+    states.remove('START') # We don't need START and STOP states - will  be handled separately
 
     neg_inf = (-math.inf)
     
-    all_paths = []
     ultimate = []
     # 'observations' are of the form [['sentence1_word1', 'sentence1_word2', 'sentence1_word3'], ['sentence2_word1']]
     for observation in observations: 
@@ -22,33 +20,32 @@ def viterbi(emission_counts, transition_counts, observations, train_obs):
         n = len(observation)
         
         path_list = []
-        
         for j in range(n): 
-            # print(j)
             pi_scores[j+1] = {}
+
+            # If an unseen word occurs, replace with '#UNK#' token
             if observation[j] not in train_obs:
                 x = "#UNK#"
             else:
                 x = observation[j]
-            # print(x)
+            
+            # Transition from 'START' to first state and emission of first state to first word 
             if j==0:
                 score_list = []
                 for u in states:
-                    emission_param = estimate_emission_param(emission_counts, x, u)
-                    # emission_param = 0.3
                     # To avoid underflow issue, we take log of emission and transition params
-                    if emission_param == 0:
-                        log_emission_param = neg_inf
-                    else:
-                        log_emission_param = math.log(emission_param)
-        
                     transition_param = get_transition_parameters(transition_counts, 'START', u)
                     if transition_param == 0:
                         log_transition_param = neg_inf
                     else:
                         log_transition_param = math.log(transition_param)
 
-                    # transition_param = 0.8
+                    emission_param = estimate_emission_param(emission_counts, x, u)
+                    if emission_param == 0:
+                        log_emission_param = neg_inf
+                    else:
+                        log_emission_param = math.log(emission_param)
+        
                     # Since we took log, we add pi_score + emission probablity + transition probability (not multiply)
                     # pi_scores[0]['START'] = 1 and log(1) = 0 so we ignore this term
                     pi_scores[j+1][u] = log_emission_param + log_transition_param
@@ -57,9 +54,8 @@ def viterbi(emission_counts, transition_counts, observations, train_obs):
 
             else:
                 for u in states:
-                    emission_param = estimate_emission_param(emission_counts, x, u)
-                    # emission_param = 0.3
                     # To avoid underflow issue, we take log of emission and transition params
+                    emission_param = estimate_emission_param(emission_counts, x, u)
                     if emission_param == 0:
                         log_emission_param = neg_inf
                     else:
@@ -67,15 +63,13 @@ def viterbi(emission_counts, transition_counts, observations, train_obs):
 
                     score_list = []
                     for v in states:
-                        # print(v)
-                        # print('pi_scores',pi_scores)
                         transition_param =  get_transition_parameters(transition_counts, v, u)
                         if transition_param == 0:
                             log_transition_param = neg_inf
                         else:
                             log_transition_param = math.log(transition_param)
+
                         # Since we took log, we add pi_score + emission probablity + transition probability (not multiply)
-                        # print(j)
                         score_list.append(pi_scores[j][v] + log_emission_param + log_transition_param)
 
                     max_score = max(score_list)
