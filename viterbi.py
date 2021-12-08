f5927f3fcb728715de871a7f336427847b8f9e9c
Madhumitha Balaji <65944319+Madhu-balaji-01@users.noreply.github.com>
2021-12-08 20:19:40 +0800

    Update viterbi.py

--- viterbi.py
+++ viterbi.py
@@ -31,7 +31,6 @@ def viterbi(emission_counts, transition_counts, observations, train_obs):
             
             # Transition from 'START' to first state and emission of first state to first word 
             if j==0:
-                score_list = []
                 for u in states:
                     # To avoid underflow issue, we take log of emission and transition params
                     transition_param = get_transition_parameters(transition_counts, 'START', u)
@@ -49,9 +48,8 @@ def viterbi(emission_counts, transition_counts, observations, train_obs):
                     # Since we took log, we add pi_score + emission probablity + transition probability (not multiply)
                     # pi_scores[0]['START'] = 1 and log(1) = 0 so we ignore this term
                     pi_scores[j+1][u] = log_emission_param + log_transition_param
-                    # print(pi_scores)
-                    # score_list.append(log_emission_param + log_transition_param)
 
+            # From the second word till the second last word
             else:
                 for u in states:
                     # To avoid underflow issue, we take log of emission and transition params
@@ -61,7 +59,7 @@ def viterbi(emission_counts, transition_counts, observations, train_obs):
                     else:
                         log_emission_param = math.log(emission_param)
 
-                    score_list = []
+                    score_dict = {}
                     for v in states:
                         transition_param =  get_transition_parameters(transition_counts, v, u)
                         if transition_param == 0:
@@ -70,74 +68,74 @@ def viterbi(emission_counts, transition_counts, observations, train_obs):
                             log_transition_param = math.log(transition_param)
 
                         # Since we took log, we add pi_score + emission probablity + transition probability (not multiply)
-                        score_list.append(pi_scores[j][v] + log_emission_param + log_transition_param)
+                        score_dict[v] = pi_scores[j][v] + log_emission_param + log_transition_param
+                        #score_list.append(pi_scores[j][v] + log_emission_param + log_transition_param)
 
-                    max_score = max(score_list)
+                    # Storing the maximum score over all v's
+                    max_score = max(list(score_dict.values()))
                     pi_scores[j+1][u] = max_score
-            
-                path_list.append(states[score_list.index(max_score)])
-                # print(path_list)
-        #print(pi_scores)
-        
-        # STOP state
+
+                    # Storing the v that gives maximum score
+                    path_list.append(max(score_dict, key = score_dict.get))
+
+                
+        # Transition from last state to 'STOP'
         pi_scores[n+1] = {}
-        score_list = []
-        #print(pi_scores)
+        score_dict ={}
         for u in states:
-            # print(pi_scores)
             transition_param = get_transition_parameters(transition_counts, u, 'STOP')
-            # transition_param = 0.8
             if transition_param == 0:
                 log_transition_param = neg_inf
             else:
                 log_transition_param = math.log(transition_param)
-            pi_scores[n+1][u] = pi_scores[n][u] + log_transition_param
-            score_list.append(pi_scores[n+1][u])
 
-        #print(pi_scores)
-        max_score = max(score_list)
-        
-        
-        back_tracker = ["STOP"]
-        
-        #temp = max(pi_scores[n], key=pi_scores[n].get)
-        
-        #back_tracker.insert(0,temp)
-        
-        for why_am_i_doing_this_i_want_to_go_back_to_blockchain in range(n,-1,-1):
+            pi_scores[n+1][u] = pi_scores[n][u] + log_transition_param  # No emission for STOP state
             
-            temp = max(pi_scores[why_am_i_doing_this_i_want_to_go_back_to_blockchain], key=pi_scores[why_am_i_doing_this_i_want_to_go_back_to_blockchain].get)
-            back_tracker.insert(0,temp)
-            
-        #print(back_tracker)
-        ultimate.append(back_tracker)
+        path_list.append(max(pi_scores[n+1], key = pi_scores[n+1].get))
+        print('Path', path_list)
+        # max_score = max(score_list)
+        # break
         
-    return(ultimate)    
+#         back_tracker = ["STOP"]
+        
+#         #temp = max(pi_scores[n], key=pi_scores[n].get)
+        
+#         #back_tracker.insert(0,temp)
+        
+#         for why_am_i_doing_this_i_want_to_go_back_to_blockchain in range(n,-1,-1):
+            
+#             temp = max(pi_scores[why_am_i_doing_this_i_want_to_go_back_to_blockchain], key=pi_scores[why_am_i_doing_this_i_want_to_go_back_to_blockchain].get)
+#             back_tracker.insert(0,temp)
+            
+#         #print(back_tracker)
+#         ultimate.append(back_tracker)
+#         break   
+#     return(ultimate)    
 
-        # pi_scores[j+1][u] = max_score
-#         path_list.append(states[score_list.index(max_score)])
-#         print(path_list)       
+#         # pi_scores[j+1][u] = max_score
+# #         path_list.append(states[score_list.index(max_score)])
+# #         print(path_list)       
 
-train_obs, emission_counts = emission_counting('train')
-transition_counts = transition_counting('train')
-observations = data_dump('./ES/dev.in')
-l = viterbi(emission_counts, transition_counts,  observations, train_obs)
+# train_obs, emission_counts = emission_counting('train')
+# transition_counts = transition_counting('train')
+# observations = data_dump('./ES/dev.in')
+# l = viterbi(emission_counts, transition_counts,  observations, train_obs)
 
-all_prediction = l
+# all_prediction = l
 
-with open('./ES/dev.in', "r", encoding="utf8") as f:
-            lines = f.readlines()
+# with open('./ES/dev.in', "r", encoding="utf8") as f:
+#             lines = f.readlines()
 
-with open('./ES/dev.p2.out', "w", encoding="utf8") as g:
-    for j in range(len(lines)):
-        word = lines[j].strip()
-        if word != "\n":
-            tag = all_prediction[j]
-            if(tag != "\n"):
-                g.write(word + " " + tag)
-                g.write("\n")
-            else:
-                g.write("\n")
+# with open('./ES/dev.p2.out', "w", encoding="utf8") as g:
+#     for j in range(len(lines)):
+#         word = lines[j].strip()
+#         if word != "\n":
+#             tag = all_prediction[j]
+#             if(tag != "\n"):
+#                 g.write(word + " " + tag)
+#                 g.write("\n")
+#             else:
+#                 g.write("\n")
                 
 #all_prediction = final_answers_part1('RU')
                 
