4628c4045b6b015b2dc90d8e7c4401491841d725
Madhumitha Balaji <65944319+Madhu-balaji-01@users.noreply.github.com>
2021-12-08 23:39:00 +0800

    DONE WITH VITERBI

--- viterbi.py
+++ viterbi.py
@@ -10,7 +10,7 @@ def viterbi(emission_counts, transition_counts, observations, train_obs):
 
     neg_inf = (-math.inf)
     
-    ultimate = []
+    ultimate_path = []
     # 'observations' are of the form [['sentence1_word1', 'sentence1_word2', 'sentence1_word3'], ['sentence2_word1']]
     for observation in observations: 
         # Initializing pi scores
@@ -44,11 +44,11 @@ def viterbi(emission_counts, transition_counts, observations, train_obs):
                         log_emission_param = neg_inf
                     else:
                         log_emission_param = math.log(emission_param)
-        
+
                     # Since we took log, we add pi_score + emission probablity + transition probability (not multiply)
                     # pi_scores[0]['START'] = 1 and log(1) = 0 so we ignore this term
                     pi_scores[j+1][u] = log_emission_param + log_transition_param
-
+               
             # From the second word till the second last word
             else:
                 for u in states:
@@ -59,7 +59,7 @@ def viterbi(emission_counts, transition_counts, observations, train_obs):
                     else:
                         log_emission_param = math.log(emission_param)
 
-                    score_dict = {}
+                    score_list = []
                     for v in states:
                         transition_param =  get_transition_parameters(transition_counts, v, u)
                         if transition_param == 0:
@@ -68,58 +68,43 @@ def viterbi(emission_counts, transition_counts, observations, train_obs):
                             log_transition_param = math.log(transition_param)
 
                         # Since we took log, we add pi_score + emission probablity + transition probability (not multiply)
-                        score_dict[v] = pi_scores[j][v] + log_emission_param + log_transition_param
-                        #score_list.append(pi_scores[j][v] + log_emission_param + log_transition_param)
+                        score_list.append(pi_scores[j][v] + log_emission_param + log_transition_param)
 
                     # Storing the maximum score over all v's
-                    max_score = max(list(score_dict.values()))
+                    max_score = max(score_list)
                     pi_scores[j+1][u] = max_score
 
-                    # Storing the v that gives maximum score
-                    path_list.append(max(score_dict, key = score_dict.get))
+                # # Storing the v that gives maximum score
+                # path_list.append(max(score_dict, key = score_dict.get))
 
                 
         # Transition from last state to 'STOP'
         pi_scores[n+1] = {}
-        score_dict ={}
         for u in states:
             transition_param = get_transition_parameters(transition_counts, u, 'STOP')
             if transition_param == 0:
                 log_transition_param = neg_inf
             else:
                 log_transition_param = math.log(transition_param)
-
+             
             pi_scores[n+1][u] = pi_scores[n][u] + log_transition_param  # No emission for STOP state
+        
+        # Backward algorithm
+        back_tracker = ["STOP"]
+        
+        for u_star in range(n,-1,-1):
+            temp = max(pi_scores[u_star], key=pi_scores[u_star].get)
+            back_tracker.insert(0,temp)
             
-        path_list.append(max(pi_scores[n+1], key = pi_scores[n+1].get))
-        print('Path', path_list)
-        # max_score = max(score_list)
-        # break
+        ultimate_path.append(back_tracker) 
+        # print('Ultimate', ultimate_path)
         
-#         back_tracker = ["STOP"]
-        
-#         #temp = max(pi_scores[n], key=pi_scores[n].get)
-        
-#         #back_tracker.insert(0,temp)
-        
-#         for why_am_i_doing_this_i_want_to_go_back_to_blockchain in range(n,-1,-1):
-            
-#             temp = max(pi_scores[why_am_i_doing_this_i_want_to_go_back_to_blockchain], key=pi_scores[why_am_i_doing_this_i_want_to_go_back_to_blockchain].get)
-#             back_tracker.insert(0,temp)
-            
-#         #print(back_tracker)
-#         ultimate.append(back_tracker)
-#         break   
-#     return(ultimate)    
+    return(ultimate_path)    
 
-#         # pi_scores[j+1][u] = max_score
-# #         path_list.append(states[score_list.index(max_score)])
-# #         print(path_list)       
-
-# train_obs, emission_counts = emission_counting('train')
-# transition_counts = transition_counting('train')
-# observations = data_dump('./ES/dev.in')
-# l = viterbi(emission_counts, transition_counts,  observations, train_obs)
+train_obs, emission_counts = emission_counting('train')
+transition_counts = transition_counting('train')
+observations = data_dump('./ES/dev.in')
+print(viterbi(emission_counts, transition_counts,  observations, train_obs))
 
 # all_prediction = l
 
