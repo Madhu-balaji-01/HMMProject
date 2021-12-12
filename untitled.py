8c65573f4c724172fdd82fe87539547d0490d29f
SHSR2001 <shyams.aat@gmail.com>
2021-12-12 16:14:35 +0800

    please send help

--- untitled.py
+++ untitled.py
@@ -11,7 +11,7 @@ def viterbi_5(emission_counts, transition_counts, sequence, states):
     
     pi_scores = {}
     pi_scores[0] = {"START": [[1,[]]]}
-    
+    states.remove("START") 
         
     for obs in sequence:
         pi_scores[temp] = {}
@@ -19,14 +19,17 @@ def viterbi_5(emission_counts, transition_counts, sequence, states):
         if obs not in train_obs:
             obs = "#UNK#"
             
+           
         for state in states:
-            params_dict = {}
             
+            params_dict = {}
+        
+
             for state_prev in pi_scores[temp-1].keys():
-                
                 emission_param = estimate_emission_param(emission_counts, obs, state)
                 if emission_param == 0:
                     log_emission_param = neg_inf
+                
                 else:
                     log_emission_param = math.log(emission_param)
                     
@@ -36,26 +39,24 @@ def viterbi_5(emission_counts, transition_counts, sequence, states):
                 else:
                     log_transition_param = math.log(transition_param)
                     
-                if (log_transition_param != neg_inf) and (log_transition_param != neg_inf):
-                    
-                    for i in range(len(pi_scores[temp-1])):
-                        params_dict[pi_scores[temp-1][state_prev][i][1]+[state_prev]] = pi_scores[temp-1][state_prev][i][0] + log_transition_param + log_emission_param
-                        
+                if (log_transition_param == neg_inf) or (log_transition_param == neg_inf):
+                    params_dict[tuple(pi_scores[temp-1][state_prev][i][1]+[state_prev])] = neg_inf
                 else:
-                    params_dict[pi_scores[temp-1][state_prev][i][1]+[state_prev]] = neg_inf
+                    for i in range(len(pi_scores[temp-1][state_prev])):
+                        params_dict[tuple(pi_scores[temp-1][state_prev][i][1]+[state_prev])] = pi_scores[temp-1][state_prev][i][0]  + log_transition_param + log_emission_param
+                                      
                     
                 # Key - 0, Values - 1
             all_probs = [0, sorted(params_dict.values(), reverse = True)]
+            
             all_probs[0] = sorted(params_dict, key=params_dict.get, reverse=True)
-                
-            if len(all_probs[1] == 1):
+            if len(all_probs[1]) == 1:
                 pi_scores[temp][state] = [[all_probs[1][0], list(all_probs[0][0])]]
                     
             else:
-                pi_scores[temp][state] = [[all_probs[1][0], list(all_probs[0][0])], [all_probs[1][1], list(all_probs[0][1])], [all_probs[1][2], list(all_probs[0][2])]]
-                    
+                pi_scores[temp][state] = [[all_probs[1][0], list(all_probs[0][0])], [all_probs[1][1], list(all_probs[0][1])], [all_probs[1][2], list(all_probs[0][2])], [all_probs[1][3], list(all_probs[0][3])], [all_probs[1][4], list(all_probs[0][4])]]
             
-        temp = temp + 1
+        temp += 1
                 
             
     params_dict = {}
@@ -66,29 +67,53 @@ def viterbi_5(emission_counts, transition_counts, sequence, states):
         else:
             log_transition_param = math.log(transition_param)
                 
-        if log_transition_params != neg_inf:
-            for i in range(pi_scores[temp-1][state_prev]):
-                params_dict[pi_scores[temp-1][state_prev][i][1] + [state_prev]] = pi_scores[temp-1][state_prev][i][0] + log_transition_param
+        if log_transition_param != neg_inf:
+            for i in range(len(pi_scores[temp-1][state_prev])):
+                    params_dict[tuple(pi_scores[temp-1][state_prev][i][1] + [state_prev])] = pi_scores[temp-1][state_prev][i][0]  + log_transition_param
                     
         else:
-            for i in range(pi_scores[temp-1][state_prev]):
-                params_dict[pi_scores[temp-1][state_prev][i][1] + [state_prev]] = neg_inf
+            for i in range(len(pi_scores[temp-1][state_prev])):
+                    params_dict[tuple(pi_scores[temp-1][state_prev][i][1] + [state_prev])] = neg_inf
                     
     all_probs = [0, sorted(params_dict.values(), reverse = True)]
     all_probs[0] = sorted(params_dict, key=params_dict.get, reverse=True)
             
-    viterbi5 = all_probs[0][2]
+    viterbi5 = all_probs[0][0]
             
-    prediction = []
-    for ind, observation in enumerate(sequence):
-        prediction.append([observation, viterbi5[ind+1]])
+    prediction = [[obs, viterbi5[ind+1]] for ind, obs in enumerate(sequence)]
+    for ind, obs in enumerate(sequence):
+        prediction.append([obs, viterbi5[ind+1]])
                 
             
     return prediction
-
+            
+    
 
 train_obs, emission_counts = emission_counting('./ES/train')
 transition_counts = transition_counting('./ES/train')
+states = list(transition_counts.keys())
 sequence = data_dump('./ES/dev.in')
-viterbi_outputs = viterbi5(emission_counts, transition_counts,  sequence, train_obs)
+sequences = []
+for i in sequence:
+    for j in i: 
+        sequences.append(j)
+viterbi_outputs = viterbi_5(emission_counts, transition_counts,  sequences, states)
+
+with open('./ES/dev.in', "r", encoding="utf8") as f:
+            lines = f.readlines()
+
+with open('./ES/dev.p69.out', "w", encoding="utf8") as g:
+    k = 0
+    num_lines = 0
+    for j in range(len(lines)):
+        word = lines[j].strip()
+
+        
+        if (word != ""):
+            path = viterbi_outputs[k][1]
+            g.write(word + " " + path)
+            g.write("\n")
+            k += 1   
             
+        else:
+            g.write("\n")
